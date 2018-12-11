6e6dca425ac2580c9beafe8222b67161d92e6edf
saad boudfor <saad.boudfor@capgemini.com>
2018-12-11 23:13:36 +0100

    004 : dynamiser l'affichage du header en fonction du scroll sur le site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit{
-  public state = '';
-  ngOnInit(): void {
+export class AppComponent {
+  public state: string = 'top';
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.scrollY === 0) {
+      this.state = 'top'
+    } else {
+      this.state = 'scrolling'
+    }
   }
 }
